f3d3e1084c0c48aea3cda134a8f3b3b22e9ab5dd
batrabhavna <Batrabhavna@outlook.com>
2023-06-18 23:57:36 +0530

    changes

--- NAGPAssignment/Registration.cs
+++ NAGPAssignment/Registration.cs
@@ -18,7 +18,7 @@ namespace NAGPAssignment
             var password = configuration.GetConnectionString("SA_PASSWORD");
             //"Server=tcp:sql-server-pod.applicationnamespace.svc.cluster.local,1433;Database=EmployeeDb;Persist Security Info=True;User Id=sa;Password=Welcome@0001234567;";
             //tcp:34.70.101.64,1433
-            string connString = $"Server=tcp:mssql-0.mssql-db.applicationnamespace.svc.cluster.local,1433;Initial Catalog=EmployeeDb;User Id=sa;Password=Welcome@0001234567;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=false";
+            string connString = $"Server=tcp:mssql-db.applicationnamespace.svc.cluster.local,1433;Initial Catalog=EmployeeDb;User Id=sa;Password=Welcome@0001234567;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=false";
             //string connString = $"Server=tcp:{host},{port};Initial Catalog=EmployeeDb;Persist Security Info=False;User Id={userid};Password={password};TrustServerCertificate=False;Connection Timeout=30;";
 
             Console.WriteLine(connString);
