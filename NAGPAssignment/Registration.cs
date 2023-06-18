ae8fecd5a0be088f2cf3fe0762531ae754acca6b
batrabhavna <Batrabhavna@outlook.com>
2023-06-19 02:06:14 +0530

    changes

--- NAGPAssignment/Registration.cs
+++ NAGPAssignment/Registration.cs
@@ -10,17 +10,22 @@ namespace NAGPAssignment
         public static IServiceCollection RegisterService(this IServiceCollection services, IConfiguration configuration)
         {
             Console.WriteLine("ASPNETCORE_ENVIRONMENT" + configuration["ASPNETCORE_ENVIRONMENT"]);
-            Console.WriteLine("ConnectionStrings.SA_HOST" + configuration["ConnectionStrings.SA_HOST"]);
-            Console.WriteLine("ConnectionStrings.SA_PORT" + configuration["ConnectionStrings.SA_PORT"]);
-            Console.WriteLine("ConnectionStrings.SA_USER" + configuration["ConnectionStrings.SA_USER"]);
+            Console.WriteLine("ConnectionStrings__SA_HOST -" + configuration["ConnectionStrings__SA_HOST"]);
+            Console.WriteLine("ConnectionStrings__SA_PORT - " + configuration["ConnectionStrings__SA_PORT"]);
+            Console.WriteLine("ConnectionStrings__SA_USER - " + configuration["ConnectionStrings__SA_USER"]);
+            Console.WriteLine("ConnectionStrings__SA_PASSWORD - " + configuration["ConnectionStrings__SA_PASSWORD"]);
+
+            Console.WriteLine("GetConnectionString -" + configuration.GetConnectionString("SA_HOST"));
+            Console.WriteLine("GetConnectionString - " + configuration.GetConnectionString("SA_PORT"));
+            Console.WriteLine("GetConnectionString - " + configuration.GetConnectionString("SA_USER"));
+            Console.WriteLine("GetConnectionString - " + configuration.GetConnectionString("SA_PASSWORD"));
 
             var host = configuration.GetConnectionString("SA_HOST");
-
             var port = configuration.GetConnectionString("SA_PORT");
             var userid = configuration.GetConnectionString("SA_USER");
             var password = configuration.GetConnectionString("SA_PASSWORD");
 
-            string connStringTest = $"Server=tcp:{host},{port};Initial Catalog=EmployeeDb;User Id=sa;Password={password};Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=false";
+            string connStringTest = $"Server=tcp:{host},{port};Initial Catalog=EmployeeDb;User Id={userid};Password={password};Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=false";
             Console.WriteLine(connStringTest);
             //"Server=tcp:sql-server-pod.applicationnamespace.svc.cluster.local,1433;Database=EmployeeDb;Persist Security Info=True;User Id=sa;Password=Welcome@0001234567;";
             //tcp:34.70.101.64,1433
