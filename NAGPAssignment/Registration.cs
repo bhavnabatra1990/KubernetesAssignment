6f488cc0538d64742edfcb4902ea8ba7850e8c6f
batrabhavna <Batrabhavna@outlook.com>
2023-06-19 01:58:16 +0530

    changes

--- NAGPAssignment/Registration.cs
+++ NAGPAssignment/Registration.cs
@@ -9,13 +9,19 @@ namespace NAGPAssignment
 
         public static IServiceCollection RegisterService(this IServiceCollection services, IConfiguration configuration)
         {
-            Console.WriteLine(configuration["ASPNETCORE_ENVIRONMENT"]);
-            Console.WriteLine(configuration["ConnectionStrings__SA_HOST"]);
+            Console.WriteLine("ASPNETCORE_ENVIRONMENT" + configuration["ASPNETCORE_ENVIRONMENT"]);
+            Console.WriteLine("ConnectionStrings.SA_HOST" + configuration["ConnectionStrings.SA_HOST"]);
+            Console.WriteLine("ConnectionStrings.SA_PORT" + configuration["ConnectionStrings.SA_PORT"]);
+            Console.WriteLine("ConnectionStrings.SA_USER" + configuration["ConnectionStrings.SA_USER"]);
+
             var host = configuration.GetConnectionString("SA_HOST");
 
             var port = configuration.GetConnectionString("SA_PORT");
             var userid = configuration.GetConnectionString("SA_USER");
             var password = configuration.GetConnectionString("SA_PASSWORD");
+
+            string connStringTest = $"Server=tcp:{host},{port};Initial Catalog=EmployeeDb;User Id=sa;Password={password};Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=false";
+            Console.WriteLine(connStringTest);
             //"Server=tcp:sql-server-pod.applicationnamespace.svc.cluster.local,1433;Database=EmployeeDb;Persist Security Info=True;User Id=sa;Password=Welcome@0001234567;";
             //tcp:34.70.101.64,1433
             string connString = $"Server=tcp:mssql-db.applicationnamespace.svc.cluster.local,1433;Initial Catalog=EmployeeDb;User Id=sa;Password=Welcome@0001234567;Trusted_Connection=True;TrustServerCertificate=True;Integrated Security=false";
